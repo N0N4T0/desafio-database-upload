0d5b20d46a03cb3e6dd228a83e791c6aff154cfb
Lucio Nonato <luciononato@hotmail.com>
2020-04-29 15:06:21 -0300

    1 commit
    desafio concluido

---
+++ src/config/upload.ts
@@ -0,0 +1,20 @@
+// armazena minhas configurações do meu upload de imagens ou arquivos dentro da aplicação
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,35 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (partial, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            partial.income += Number(transaction.value);
+            break;
+
+          case 'outcome':
+            partial.outcome += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return partial;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
